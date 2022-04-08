5535d8a3d134ebd6f45378575938130b6b913124
Hossam Qandel <hossamegyqandel@gmail.com>
2022-04-08 16:27:39 +0200

    A new Log E222222222

--- app/src/main/java/com/learn/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/learn/daggerhilt/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.e(TAG, "onCreate: ", )
+        Log.e(TAG, "onCreate222222222222: ", )
 
 
         Toast.makeText(baseContext, human.getEat(), Toast.LENGTH_LONG).show()
